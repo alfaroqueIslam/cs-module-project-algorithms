35a62504bdf8f409b38e1fa2c7506b0c49e63e85
Alfaroque Islam <alfaroque5@gmail.com>
2020-09-16 21:37:25 -0400

    CS Unit 1 Week 3 Assignment 3

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -1,147 +1,62 @@
-from collections import deque
 
-
-class BSTNode:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-        self.level = None
-
-    # Insert the given value into the tree
-    def insert(self, value):
-        if self.value:
-            if value < self.value:
-                if self.left is None:
-                    self.left = BSTNode(value)
-                else:
-                    self.left.insert(value)
-            elif value >= self.value:
-                if self.right is None:
-                    self.right = BSTNode(value)
-                else:
-                    self.right.insert(value)
-        else:
-            self.value = value
-
-    # Return True if the tree contains the value
-    # False if it does not
-    def contains(self, target):
-        if target < self.value:
-            if self.left is None:
-                return False
-            return self.left.contains(target)
-        elif target > self.value:
-            if self.right is None:
-                return False
-            return self.right.contains(target)
-        else:
-            return True
-
-    # Return the maximum value found in the tree
-    def get_max(self):
-        if self.right == None:
-            return self.value
-        else:
-            return self.right.get_max()
-
-    # Call the function `fn` on the value of each node
-    def for_each(self, fn):
-        if self.left:
-            self.left.for_each(fn)
-        fn(self.value),
-        if self.right:
-            self.right.for_each(fn)
-
-    # Part 2 -----------------------
-
-    # Print all the values in order from low to high
-    # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self):
-        if self.left:
-            self.left.in_order_print()
-        print( self.value),
-        if self.right:
-            self.right.in_order_print()
-
-    # Print the value of every node, starting with the given node,
-    # in an iterative breadth first traversal
-    def bft_print(self):
-        self.level = 1
-        queue = deque([self])
-        output = []
-        current_level = self.level
-
-        while len(queue)>0:
-
-            current_node = queue.popleft()
-
-            if(current_node.level >= current_level):
-                output.append("\n")
-                current_level += 1
-
-            print(str(current_node.value))
-
-            if current_node.left != None:
-                current_node.left.level = current_level + 1 
-                queue.append(current_node.left)
-            
-
-
-            if current_node.right != None:
-                current_node.right.level = current_level + 1 
-                queue.append(current_node.right)
- 
-
-    # Print the value of every node, starting with the given node,
-    # in an iterative depth first traversal
-    def dft_print(self):
-        self.level = 1
-        stack = deque([self])
-        current_level = self.level
-
-
-        while len(stack)>0:
-
-            current_node = stack.pop()
-
-            if(current_node.level >= current_level):
-                current_level += 1
-
-            print(str(current_node.value))
-
-            if current_node.left != None:
-                current_node.left.level = current_level + 1 
-                stack.append(current_node.left)
-            
-
-
-            if current_node.right != None:
-                current_node.right.level = current_level + 1 
-                stack.append(current_node.right)
-        
-    # Stretch Goals -------------------------
-    # Note: Research may be required
-
-    # Print Pre-order recursive DFT
-    def pre_order_dft(self):
-        pass
-
-    # Print Post-order recursive DFT
-    def post_order_dft(self):
-        pass
 '''
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+from collections import deque
+
 def sliding_window_max(nums, k):
-    c= 0
-    l = 0
-    BSTNode n = new BSTNode()
-    for i in range(len(nums)):
-        c = c + 1
+    """ Create a Double Ended Queue, Qi that  
+    will store indexes of array elements.  
+    The queue will store indexes of useful  
+    elements in every window and it will 
+    maintain decreasing order of values from 
+    front to rear in Qi, i.e., arr[Qi.front[]] 
+    to arr[Qi.rear()] are sorted in decreasing 
+    order"""
+    Qi = deque()
+    lst = [] 
+      
+    # Process first k (or first window)  
+    # elements of array 
+    for i in range(k): 
         
-        lst.append(max)
+        # For every element, the previous  
+        # smaller elements are useless 
+        # so remove them from Qi 
+        while Qi and nums[i] >= nums[Qi[-1]] : 
+            Qi.pop() 
+          
+        # Add new element at rear of queue 
+        Qi.append(i); 
+          
+    # Process rest of the elements, i.e.  
+    # from arr[k] to arr[n-1] 
+    for i in range(k, len(nums)): 
+          
+        # The element at the front of the 
+        # queue is the largest element of 
+        # previous window, so print it 
+        lst.append(nums[Qi[0]]) 
+          
+        # Remove the elements which are  
+        # out of this window 
+        while Qi and Qi[0] <= i-k: 
+              
+            # remove from front of deque 
+            Qi.popleft()  
+          
+        # Remove all elements smaller than 
+        # the currently being added element  
+        # (Remove useless elements) 
+        while Qi and nums[i] >= nums[Qi[-1]] : 
+            Qi.pop() 
+          
+        # Add current element at the rear of Qi 
+        Qi.append(i) 
+      
+    # Print the maximum element of last window 
+    lst.append(nums[Qi[0]])
 
     return lst
 
